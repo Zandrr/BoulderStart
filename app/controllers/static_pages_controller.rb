a9a94d37c8278e34c721e0900dc2bd1e534c695f
Alex Campbell <alexander.campbell@colorado.edu>
2014-02-18 14:46:59 -0700

    website added

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def resources
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
+  def campus
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '3c0168100853fb9832680b42a257231005399453e1deb54bb53276a668a0511764169cc6685b7fefd32fca92445624e9989a9c0a6729cb19292fd0a03c55e14b'
