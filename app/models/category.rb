924cf0d5d5f5899eee80535fc9c43187bc76874a
Fletcher Richman <frichman3@gmail.com>
2014-03-15 14:34:25 -0600

    added acts as taggable on

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+  belongs_to :users
+  belongs_to :positions
+
+  acts_as_taggable
+  acts_as_taggable_on :interests
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :positions
+  has_many :categories
+
+  acts_as_tagger
 end
