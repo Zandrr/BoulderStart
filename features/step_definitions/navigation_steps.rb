ceaa9a69dd24ceabed6e15a2d9ed513e295c81b5
Fletcher Richman <frichman3@gmail.com>
2014-04-14 22:24:47 -0600

    company add position test passing

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -2,6 +2,18 @@ Given /^I am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
+Given /^I am a new, authenticated (.+)$/ do |orgtype|
+  email = 'testing@man.net'
+  password = 'secretpass'
+  User.new(:email => email, :password => password, :password_confirmation => password, :orgtype => orgtype).save!
+
+  visit '/users/sign_in'
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  click_button "Sign in"
+
+end
+
 When /^I fill in "([^\"]*)" with "([^\"]*)"$/ do |field, value|
   fill_in(field, with: value)
 end
@@ -14,9 +26,10 @@ end
 When /^I press "([^\"]*)"$/ do |button|
   click_button(button)
 end
-	Then /^I should be on (.+)$/ do |page_name|
+
+Then /^I should be on (.+)$/ do |page_name|
   current_path.should == path_to(page_name)
-	end
+end
 
 Then (/^(?:|I )should see "([^"]*)"$/) do |text|
   if page.respond_to? :should
