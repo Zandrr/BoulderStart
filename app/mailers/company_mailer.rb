3e3b8819df02ba8e0c436b18523fd441f633be2e
Zandrr <alexander.campbell@colorado.edu>
2014-04-24 00:10:30 -0600

    emails will send to all users that are startups

--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -1,14 +1,19 @@
 class CompanyMailer < ActionMailer::Base
  default from: "startups2students@gmail.com"
  #include SendGrid
-# @startups = User.find_all_by_orgtype('startup').email
-# @maillist = []
-# 	@startups.each do |org|
-# 		@maillist << org.email
-	# end
+
+def email_list
+  @startups = User.find_all_by_orgtype('startup')
+  @maillist = []
+  @startups.each do |org|
+  		@maillist << org.email
+  end
+  return @maillist
+end
 
  def resume_mailer(user)
- 	mail(to: 'alexande.campbell@cololrado.edu', subject: 'WE CAN SEND EMAIL')
+
+ 	mail(to: email_list, subject: 'Your weekly candidate digest.')
  end
 
 
