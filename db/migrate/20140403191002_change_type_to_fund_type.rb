5d093a26410fdf270e0b2d1c9ae74bf059901289
Zandrr <alexander.campbell@colorado.edu>
2014-04-27 17:30:33 -0600

    fixed migrations

--- db/migrate/20140403191002_change_type_to_fund_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTypeToFundType < ActiveRecord::Migration
-  def change
-  	change_column :fundings, :type,  :funding_type
-  end
-end
--- db/migrate/20140403191213_change_type_to_funding_type.rb
+++
@@ -1,4 +0,0 @@
-class ChangeTypeToFundingType < ActiveRecord::Migration
-  def change
-  end
-end
