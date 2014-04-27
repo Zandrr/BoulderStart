fc9160fb50ee2b1f4e93e946ed417d1cff2e3c58
Zandrr <alexander.campbell@colorado.edu>
2014-04-27 17:34:28 -0600

    fix migration

--- db/migrate/20140410182403_add_columns_to_blogs.rb
+++ db/migrate/20140410182403_add_columns_to_blogs.rb
@@ -3,5 +3,5 @@ class AddColumnsToBlogs < ActiveRecord::Migration
     add_column :blogs, :content, :text
     add_column :blogs, :user_id, :integer
   end
-  add_index :blogs, [:user_id, :created_at]
+  
 end
