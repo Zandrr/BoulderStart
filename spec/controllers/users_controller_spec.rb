10d01f27d6af49e6e3006bc8d6b7debe010e7f10
Fletcher Richman <frichman3@gmail.com>
2014-05-05 01:09:43 -0600

    updated and sexy rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,19 +2,63 @@ require 'spec_helper'
 
 describe UsersController do
 
+
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success
     end
+
+  end
+
+  describe "logic on index" do 
+
+    before(:each) do 
+      @user = FactoryGirl.create(:user)
+      @tech_user = FactoryGirl.create(:user_tech)
+      @nontech_user = FactoryGirl.create(:user_nontech)
+    end
+
+    it "shows a valid candidate" do
+      get 'index'
+      assigns(:candidates).first.should eq(@user)
+    end
+
+    it "shows all candidates with no filter" do 
+      get 'index'
+      assigns(:candidates).size.should eq(3)
+    end
+
+    it "only shows a only shows a technical candidate when filtered" do 
+      get 'index', {:filter => 'Technical'}
+      assigns(:candidates).first.should eq(@tech_user)
+    end
+
+    it "only shows a non-technical candidate when filtered" do 
+      get 'index', {:filter => 'Non-Technical'}
+      assigns(:candidates).first.should eq(@nontech_user)
+    end
+
+
+  end
+
+  describe "negative logic on index" do 
+
+    it "doesn't show another startup" do
+      get 'index'
+      FactoryGirl.create(:user_startup)
+      assigns(:candidates).should be_empty
+    end
   end
 
   describe "GET 'show'" do
+
     it "returns http success" do
-      user = FactoryGirl.create(:user)
-      get 'show', id: user.id
+      @user = FactoryGirl.create(:user)
+      get 'show', id: @user.id
       response.should be_success
     end
+
   end
 
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,23 @@ FactoryGirl.define do
   factory :user do
     name "John"
     email "john@example.com"
-    password "passoword"
+    password "password"
     password_confirmation "password"
+    orgtype "candidate"
+  end
+
+  factory :user_startup, :parent => :user do
+    orgtype "company"
+  end
+
+  factory :user_tech, :parent => :user do
+    email "john_tech@example.com"
+    discipline "Technical"
+  end
+
+  factory :user_nontech, :parent => :user do
+    email "john_nontech@example.com"
+    discipline "Non-Technical"
   end
 
 end
