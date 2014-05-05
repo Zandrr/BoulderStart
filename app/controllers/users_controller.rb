a91e9f8b1e9e49d903fc42f21f84e9f02c437c27
Fletcher Richman <frichman3@gmail.com>
2014-05-05 00:35:28 -0600

    forgot to merge this

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,30 +3,21 @@ class UsersController < ApplicationController
 
   def index
   	if params.has_key?(:filter)
-<<<<<<< HEAD
   		@candidates = User.where(:orgtype => 'candidate').find_all_by_discipline(params[:filter])
   	else
   		@candidates = User.where(:orgtype => 'candidate')
-  	end
-=======
-      @candidates = User.where(:orgtype => 'candidate').find_all_by_discipline(params[:filter])
-    else
-      @candidates = User.where(:orgtype => 'candidate')
->>>>>>> features/testing
+    end
   end
 
   def show
     @user = User.find(params[:id])
-<<<<<<< HEAD
     @blogs = @user.blogs
   end
 
   def weekly_emails
     CompanyMailer.resume_mailer.deliver
     redirect_to root_path, :notice => "Emails delivered!"
-=======
     #@blogs = @user.blogs
->>>>>>> features/testing
   end
 
 end
