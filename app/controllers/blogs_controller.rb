71bbed89fef94890fb4edef5c8ae8ab150f2d00d
Zandrr <alexander.campbell@colorado.edu>
2014-09-07 18:50:22 -0600

    fixed blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,8 +9,8 @@ class BlogsController < ApplicationController
   
 
   @client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = "acupICugan9LNSpGPpg"
-    config.consumer_secret     = "DCgAwp7ZhV5cQsbadhtfEHVHBvqUXvUiH3XmjGLJA"
+    # config.consumer_key        = "acupICugan9LNSpGPpg"
+    # config.consumer_secret     = "DCgAwp7ZhV5cQsbadhtfEHVHBvqUXvUiH3XmjGLJA"
     end
       
       @users = ["PivotDesk", "GnipEng", "SiliconFlatiron"]
--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -8,8 +8,8 @@ class TwitterController < ApplicationController
     # Certain methods require authentication. To get your Twitter OAuth credentials,
     # register an app at http://dev.twitter.com/apps
 @client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "acupICugan9LNSpGPpg"
-  config.consumer_secret     = "DCgAwp7ZhV5cQsbadhtfEHVHBvqUXvUiH3XmjGLJA"
+  # config.consumer_key        = "acupICugan9LNSpGPpg"
+  # config.consumer_secret     = "DCgAwp7ZhV5cQsbadhtfEHVHBvqUXvUiH3XmjGLJA"
     end
 
       @users = ["PivotDesk", "GnipEng", "SiliconFlatiron"]
